7ad98614ec61e6324e405ca61ea02c27efbf452e
David H <iq.davidh@gmail.com>
2019-05-17 22:48:25 -0500

    iniciando

---
+++ app.js
@@ -0,0 +1,40 @@
+const app = new Vue({
+    el: '#app',
+    data: {
+        msg: 'yea_baby',
+        lista: [
+            {nombre: 'manzana', q: 0},
+            {nombre: 'fresa', q: 2}
+        ],
+        nuevaFruta: {nombre: '', q: 0},
+        total: 0
+    },
+    methods: {
+
+        agregarFruta() {
+            this.lista.push(
+                {
+                    nombre: this.nuevaFruta.nombre,
+                    q: parseInt(this.nuevaFruta.q)
+                }
+            );
+
+            this.nuevaFruta.nombre = '';
+            this.nuevaFruta.q = '';
+        }
+
+    },
+    computed: {
+        sumarFrutas() {
+
+            this.total = this.lista
+                .reduce((total, item) => {
+                    total += item.q;
+                    return total;
+                }, 0)
+            ;
+
+            return this.total;
+        }
+    }
+});
